2d5c5034558fd99f4882b101d3095505cc70bda6
hjj0820 <jeje186260820@gmail.com>
2025-05-20 15:34:00 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+Current workspace avatar
+hjj0820
+My apps
+My profile
+Explore
+Discuss
+Back
+Deploy an app
+Repository  
+Paste GitHub URL
+hjj0820/25-aiproject0
+Branch
+main
+This branch does not exist
+
+Main file path
+dateffokdqwd
+This file is not a valid Python script
+
+App URL (optional)
+25fur812u8r9fhu
+.streamlit.app
+Domain is available
+
+Advanced settings
+Deploy
